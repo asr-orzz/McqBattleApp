9530197d52d8b5deedbde4799a60dd45c07f97d6
Aneesh Singh Rajoriya <aneeshsinghrajoriya@gmail.com>
2025-05-22 16:28:45 +0530

    Added .env.example file

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,7 +9,6 @@ app.use(express.json());
 app.use("/api/v1/user",userRouter);
 app.use("/api/v1/user/games",gameRouter);
 
-
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
 });
