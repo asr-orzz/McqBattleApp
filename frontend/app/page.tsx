49700383f3ef721a0dcaec6127ea2c81cc1b8b27
Aneesh Singh Rajoriya <aneeshsinghrajoriya@gmail.com>
2025-05-26 15:43:01 +0530

    Added Notifications by react-toastify

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,6 +1,18 @@
+"use client"
+import { toastDismissAll, toastLoading, toastSuccess } from "@/utils/toast";
+
 
 export default function Home() {
-  return (<>
-    Hi there
-  </>);
+  function hehe(){
+    toastLoading("User Created Succesfully");
+  }
+  function hehe2(){
+    toastDismissAll();
+  }
+  return (
+    <>
+      <button onClick={()=>{hehe()}}>Click Me</button>
+      <button onClick={()=>{hehe2()}}>Close all</button>
+    </>
+  );
 }
