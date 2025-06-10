ba82f5059acaa9653e45d7fe80b33e7cf6b4ec8c
Aneesh Singh Rajoriya <aneeshsinghrajoriya@gmail.com>
2025-06-10 23:31:17 +0530

    Adding cors origin

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-import cors from "cors"
+import cors from "cors";
 import express from 'express';
 import { userRouter } from './routes/userRouter';
 import { gameRouter } from "./routes/gameRouter";
@@ -12,17 +12,21 @@ import { playerRequestRouter } from "./routes/playerRequest";
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: "https://mcq-battle-app.vercel.app",
+  credentials: true
+}));
 
-app.use("/api/v1/users",userRouter);
-app.use("/api/v1/games",gameRouter);
-app.use("/api/v1/questions",questionRouter);
-app.use("/api/v1/options",optionRouter);
-app.use("/api/v1/players",playerRouter);
-app.use("/api/v1/playerRequest",playerRequestRouter);
-function main(){
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/games", gameRouter);
+app.use("/api/v1/questions", questionRouter);
+app.use("/api/v1/options", optionRouter);
+app.use("/api/v1/players", playerRouter);
+app.use("/api/v1/playerRequest", playerRequestRouter);
+
+function main() {
   app.listen(3001, () => {
-      console.log('Server is running on http://localhost:3001');
+    console.log('Server is running on http://localhost:3001');
   });
 }
 
